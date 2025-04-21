28d04e30e859b69597d70e8bc1a526e71013894b
yashjain7856 <yashjainrps012@gmail.com>
2025-04-21 22:35:33 +0530

    fixing base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",
+  base: "/yash-jain-portfolio/",
   server: {
     host: "::",
     port: 8080,
